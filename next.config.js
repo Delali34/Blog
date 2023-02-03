5fc2be1a44a5a2907b0abc03a0bd15e203f80d60
Dela <ernestanyomitse752@gmail.com>
2023-02-03 23:08:41 +0000

    revalidate

--- next.config.js
+++ next.config.js
@@ -3,3 +3,8 @@ module.exports = {
   reactStrictMode: true,
   incremental: true,
 };
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({});
